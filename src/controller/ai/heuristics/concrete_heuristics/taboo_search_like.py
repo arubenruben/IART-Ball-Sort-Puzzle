ea9c98de20c70a0fcdbf3ab464800488d9dbe31b
dmbg1 <dmbg1999@hotmail.com>
2021-03-23 23:00:56 +0000

    Third heuristic done but not functional

--- src/controller/ai/heuristics/concrete_heuristics/taboo_search_like.py
+++ src/controller/ai/heuristics/concrete_heuristics/taboo_search_like.py
@@ -3,5 +3,25 @@ from src.controller.AI.heuristics.heuristic import Heuristic
 
 class TabooSearchHeuristic(Heuristic):
     def evaluate(self, node):
-        pass
+        tubes = node.state.raw_test_tubes
+
+        total = 0
+
+        for tube in tubes:
+
+            if len(tube[0]) == 0:
+                total += 10
+                continue
+
+            tube_color = tube[0][0]
+            total += 5
+
+            for i in range(1, len(tube[0])):
+
+                if tube[0][i] == tube_color:
+                    total += 5
+                else:
+                    break
+
+        return total
 
